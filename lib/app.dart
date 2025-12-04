67436879cc92eea3e60edfa6b697a7e832ab95fd
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-12-04 19:32:23 +0545

    Project setup: Initialized folder structure, created Screens subfolders, added app.dart and main.dart setup, configured assets directory and updated pubspec.yaml with correct asset paths.

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'screens/Splash/splash_screen.dart';
+
+class KreativeKindleApp extends StatelessWidget {
+  const KreativeKindleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "KreativeKindle",
+      theme: ThemeData(fontFamily: 'Roboto'),
+      home: const SplashScreen(),
+    );
+  }
+}
