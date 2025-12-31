861917409eab56e397f49bf4de8556c321ce7f09
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-12-31 19:17:33 +0545

    feat(sprint-3): setup hive model, generator and adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+import 'app.dart';
+import 'features/auth/data/models/user_model.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  // register adapter
+  Hive.registerAdapter(UserModelAdapter());
+
   runApp(const KreativeKindleApp());
 }
